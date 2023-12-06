567bc02efcb7a20c52ed25f4ea46fb056a829c58
manicharangit <150458103+manicharangit@users.noreply.github.com>
2023-12-06 20:34:41 +0530

    Update LrnConstants.java

--- src/test/java/com/LRN/testing/util/LrnConstants.java
+++ src/test/java/com/LRN/testing/util/LrnConstants.java
@@ -4,7 +4,8 @@ package com.LRN.testing.util;
 public class LrnConstants {
 
 	public static final String URL = "url";
-	public static final String PROPERTY_LOCATION = "C:\\Users\\Mani\\eclipse-workspace\\projects\\LRN_project\\src\\main\\resources\\properties\\project.properties";
+	public static final String PROPERTY_LOCATION ="C:\\Users\\Mani\\git\\document\\src\\main\\resources\\properties\\project_jap.properties";
+	public static final String DOC_PROPERTY_LOCATION ="C:\\Users\\Mani\\git\\document\\src\\main\\resources\\properties\\document.properties";
 	public static final String BROWSER="browser";
 	public static final String DRIVER_C="driver_c";
 	public static final String DRIVER_LOCATION_C="driver_location_c";
@@ -47,7 +48,6 @@ public class LrnConstants {
 	//support_services Mapping variables
 	public static final String SUPPORT_SERVICE= "support_service";
 	public static final String TRAINING= "training";
-	public static final String DOCUMENTATION= "documentation";
 	public static final String COMMUNITY_FOR= "community_for";
 	public static final String PREMIUM_SUPP= "premium_supp";
 	public static final String REQUEST_NOTE= "request_note";
@@ -60,9 +60,62 @@ public class LrnConstants {
 	
 	//Language Mapping variables
 	public static final String ENGLISH= "english";
-	public static final String ESPANOL = "Espanol";
-
-
+	public static final String JAPANESE = "japanese";
 	
+	// tabs
+	public static final int T0 = 0;
+	public static final int T1 = 1;
+	public static final int T2 = 2;
+
+	// Documentation mapping variables
+	public static final String DOCUMENTATION= "documentation";
+	public static final String DEVELOPER_GUIDE= "developer_guide";
+	// developing tutorials
+	public static final String DEVELOPER_TUTORIALS= "developer_tutorials";
+	public static final String SYSTEM_REQUIREMENTS= "system_requirements";
+	public static final String CONFIGURING_CODE= "configuring_code";
+	public static final String CODING_STANDARDS= "coding_standards";
+	public static final String SOURCE_CODE_ORG= "source_code_org";
+	public static final String DEFAULT_DATABASE= "default_database";
+	public static final String ARCHITECTURE_OF_NOP= "architecture_of_nop";
+	public static final String INSTRUCTION_OF_NOP= "instruction_of_nop";
+	public static final String UPDATING_ENTITY= "updating_entity";
+	public static final String INVERSION_OF_CONTROL= "inversion_of_control";
+	public static final String HOW_TO_REGISTER= "how_to_register";
+	public static final String HOW_TO_MIGRATE= "how_to_migrate";
+	public static final String A_GUIDE= "a_guide";
+	public static final String DATA_VALIDATION= "data_validation";
+	public static final String SCHEDULED_TASKS= "scheduled_tasks";
+	public static final String EXPOSING= "exposing";
+	public static final String SETTINGS_API= "settings_api";
+	public static final String HOW_INTERFACE= "how_interface";
+	public static final String DESCRIPTION_OF_WEB= "description_of_web";
+	public static final String STEP_BY_STEP= "step_by_step";
+	public static final String DOCKER= "docker";
+	public static final String HOW_TO_DEPLOY= "how_to_deploy";
+	public static final String GUIDE_TO_CREATE= "guide_to_create";
+	public static final String CSP= "csp";
+	public static final String THE_SETTINGS_FROM= "the_settings_from";
+	public static final String DESCRIPTION_PLUGIN= "description_plugin";
+	public static final String DESCRIPTION_PERMISSION= "description_permission";
+	public static final String UNIT_TESTING= "unit_testing";
+	// create a nop_plugin
+	public static final String CREATE_NOP_PLUGIN= "create_nop_plugin";
+	public static final String NOP_4_60= "nop_4_60";
+	public static final String NOP_4_50= "nop_4_50";
+	public static final String NOP_4_40= "nop_4_40";
+	public static final String NOP_4_30= "nop_4_30";
+	public static final String NOP_4_20= "nop_4_20";
+	public static final String NOP_4_10= "nop_4_10";
+	public static final String NOP_4_00= "nop_4_00";
+	public static final String NOP_3_90= "nop_3_90";
+	public static final String HOW_CODE_PAYMENT= "how_code_payment";
+	public static final String HOW_CODE_COMP= "how_code_comp";
+	public static final String PLUGIN_DATA= "plugin_data";
+	public static final String PLUGIN_DATA_4_20= "plugin_data_4_20";
+	public static final String HOW_ADD_MENU= "how_add_menu";
+	public static final String ADDING_CSS_JS= "adding_css_js";
+	public static final String DES_OF_STRUCTURE= "des_of_structure";
+		
 	}
 
