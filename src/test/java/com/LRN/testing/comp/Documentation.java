cfabdb80eb530c9ae9df35a0f79f1783f49db809
sairameppakayala123 <150458218+sairameppakayala123@users.noreply.github.com>
2023-12-06 21:16:13 +0530

    Update Documentation.java

--- src/test/java/com/LRN/testing/comp/Documentation.java
+++ src/test/java/com/LRN/testing/comp/Documentation.java
@@ -75,6 +75,92 @@ public class Documentation extends LaunchApp {
 		util.click(service.getValueFromDoc(HOW_ADD_MENU));
 		util.click(service.getValueFromDoc(ADDING_CSS_JS));
 		util.click(service.getValueFromDoc(DES_OF_STRUCTURE));
+
+
+		util.click(service.getValue(INSTALLATION));
+		util.click(service.getValue(TECH));
+		util.click(service.getValue(INSTALNOP));
+		util.click(service.getValue(HOSTING));
+		util.click(service.getValue(INSTALL));
+		util.click(service.getValue(INSTALWIN));
+		util.click(service.getValue(INSTALLINUX));
+		util.click(service.getValue(INSTALMICRO));
+		util.click(service.getValue(LOAD));
+		util.click(service.getValue(PREINSTAL));
+		util.click(service.getValue(FULLYMANAGED));
+		util.click(service.getValue(UPGRADE));
+		
+		util.click(service.getValue(RUNNINGSTORE));
+		util.click(service.getValue(CATALOG));
+		util.click(service.getValue(PRODUCTS));
+		util.click(service.getValue(ADDPRODUCT));
+		util.click(service.getValue(PRODUCTATTR));
+		util.click(service.getValue(SPECIFICATION));
+		util.click(service.getValue(DOWNLOADABLE));
+		util.click(service.getValue(GROUPED));
+		util.click(service.getValue(RECURRING));
+		util.click(service.getValue(RENTAL));
+		util.click(service.getValue(IMPORTE));
+		util.click(service.getValue(PRODUCTTAGS));
+		util.click(service.getValue(PRODUCTREVIEW));
+		util.click(service.getValue(CATOGRIES));
+		util.click(service.getValue(MANUFACTURE));
+		util.click(service.getValue(CATALOGSETT));
+		
+		
+		util.click(service.getValue(ORDERMAN));
+		util.click(service.getValue(ORDERS));
+		util.click(service.getValue(SHIPPING));
+		util.click(service.getValue(RETURN));
+		util.click(service.getValue(SHOPPING));
+		util.click(service.getValue(INVENTORY));
+		util.click(service.getValue(CHECKOUT));
+		util.click(service.getValue(ORDERSETT));
+		util.click(service.getValue(CUSTOMER_MANAGEMENT));
+		util.click(service.getValue(MANAGING_CUSTOMER));
+		util.click(service.getValue(CUST_ROLES));
+		util.click(service.getValue(ACESS));
+		util.click(service.getValue(ACTIVITY));
+		util.click(service.getValue(ONLINE));
+		util.click(service.getValue(CUSTOMER));
+		util.click(service.getValue(VENDOR));
+		util.click(service.getValue(PROMO_TOOLS));
+		util.click(service.getValue(DISCOUNTS));
+		util.click(service.getValue(GIFT_CARDS));
+		util.click(service.getValue(REWARD_POINTS));
+		util.click(service.getValue(AFFILIATES));
+		util.click(service.getValue(TIER));
+		util.click(service.getValue(CROSS_SELLS));
+		util.click(service.getValue(EMAIL_CAMP));
+		util.click(service.getValue(BREVO));
+		util.click(service.getValue(BREVO_PLUG));
+		util.click(service.getValue(RECOVER_ABAND));
+		util.click(service.getValue(SEND_AN_ORDER));
+		util.click(service.getValue(FACEBOOK));
+		util.click(service.getValue(CONT_MANAGE));
+		util.click(service.getValue(BLOG));
+		util.click(service.getValue(NEWS));
+		util.click(service.getValue(FORUMS));
+		util.click(service.getValue(POLLS));
+		util.click(service.getValue(TOPICS));
+		util.click(service.getValue(MESSAGE));
+		util.click(service.getValue(SEARCH_ENGINE));
+		util.click(service.getValue(REPORTSS));
+		util.click(service.getValue(SYS_ADMIN));
+		util.click(service.getValue(SYS_INFO));
+		util.click(service.getValue(LOG));
+		util.click(service.getValue(WARNINGS));
+		util.click(service.getValue(MAINTENANCE));
+		util.click(service.getValue(MESSAGE_QUE));
+		util.click(service.getValue(SCHEDULE_TASKS));
+		util.click(service.getValue(TEMPLATES));
+		
+		
+		
+
+
+
+
 		logger.info("creating nop plugin navigation ended");
 		logger.info("developer guide navigation ended");
 		
