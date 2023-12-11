71d7188aa061fdf2c8e4b740c4fb0b84e9698caf
manicharangit <150458103+manicharangit@users.noreply.github.com>
2023-12-11 19:35:04 +0530

    Update Partners.java

--- src/test/java/com/LRN/testing/comp/Partners.java
+++ src/test/java/com/LRN/testing/comp/Partners.java
@@ -3,6 +3,8 @@ package com.LRN.testing.comp;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
 import static com.LRN.testing.util.LrnConstants.*;
 
 import com.LRN.testing.LaunchApp;
@@ -11,7 +13,7 @@ import com.LRN.testing.service.PropertyService;
 public class Partners extends LaunchApp {
 	private Logger logger = LogManager.getLogger(Partners.class.getClass());
 	RemoteWebDriver driver = null;
-	
+	@Test
 	public void navigateTo() throws Exception{
 		logger.info("partners navigation started");
 		PropertyService service = new PropertyService();
